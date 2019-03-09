013baa5f17594e250b578bbcd61373cab6cd0bd3
Max Taylor <mjttk5@mail.missouri.edu>
2019-03-08 22:34:29 -0600

    Initial and final commit

---
+++ Documents Core Data/DocumentsViewController.swift
@@ -0,0 +1,159 @@
+//
+//  DocumentsViewController.swift
+//  Documents Core Data Search
+//
+//  Created by Maxwell Taylor on 3/8/19.
+//  Copyright © 2018 Maxwell Taylor. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DocumentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var documentsTableView: UITableView!
+    let dateFormatter = DateFormatter()
+    var documents = [Document]()
+    var filteredDocuments = [Document]()
+    let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchController()
+        self.documentsTableView.tableFooterView = UIView()
+        
+        title = "Documents"
+
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchDocuments()
+        setupSearchController()
+        documentsTableView.reloadData()
+    }
+    
+    func alertNotifyUser(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel) {
+            (alertAction) -> Void in
+            print("OK selected")
+        })
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func fetchDocuments() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Document> = Document.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)] // order results by document name ascending
+        
+        do {
+            documents = try managedContext.fetch(fetchRequest)
+        } catch {
+            alertNotifyUser(message: "Fetch for documents could not be performed.")
+            return
+        }
+    }
+    
+    func deleteDocument(at indexPath: IndexPath) {
+        let document = documents[indexPath.row]
+        
+        if let managedObjectContext = document.managedObjectContext {
+            managedObjectContext.delete(document)
+            
+            do {
+                try managedObjectContext.save()
+                self.documents.remove(at: indexPath.row)
+                documentsTableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                alertNotifyUser(message: "Delete failed.")
+                documentsTableView.reloadData()
+            }
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredDocuments.count
+        }
+        return documents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
+        
+        let document: Document
+        if let cell = cell as? DocumentTableViewCell {
+            if searchController.isActive && searchController.searchBar.text != "" {
+                document = filteredDocuments[indexPath.row]
+            } else {
+                document = documents[indexPath.row]
+            }
+            cell.nameLabel.text = document.name
+            cell.sizeLabel.text = String(document.size) + " bytes"
+            
+            if let modifiedDate = document.modifiedDate {
+                cell.modifiedLabel.text = dateFormatter.string(from: modifiedDate)
+            } else {
+                cell.modifiedLabel.text = "unknown"
+            }
+        }
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DocumentViewController,
+           let segueIdentifier = segue.identifier, segueIdentifier == "existingDocument",
+           let row = documentsTableView.indexPathForSelectedRow?.row {
+                destination.document = documents[row]
+        }
+    }
+    
+    func setupSearchController() {
+        definesPresentationContext = true
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.barTintColor = UIColor(white: 0.9, alpha: 0.9)
+        searchController.searchBar.placeholder = "Search by document name"
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        documentsTableView.tableHeaderView = searchController.searchBar
+    }
+    
+    func filterRowsForSearchedText(_ searchText: String) {
+        filteredDocuments = documents.filter({(document: Document) -> Bool in
+            return document.name?.lowercased().contains(searchText.lowercased()) ?? true||document.content?.lowercased().contains(searchText.lowercased()) ?? true
+        })
+        documentsTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteDocument(at: indexPath)
+        }
+    }
+
+}
+
+extension DocumentsViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let term = searchController.searchBar.text {
+            filterRowsForSearchedText(term)
+        }
+    }
+}
---
+++ Documents Core Data/Model/Document+CoreDataClass.swift
@@ -0,0 +1,53 @@
+//
+//  Document+CoreDataClass.swift
+//  Documents Core Data Search
+//
+//  Created by Maxwell Taylor on 3/8/19.
+//  Copyright © 2018 Maxwell Taylor. All rights reserved.
+//
+//
+
+import UIKit
+import CoreData
+
+@objc(Document)
+public class Document: NSManagedObject {
+    var modifiedDate: Date? {
+        get {
+            return rawModifiedDate as Date?
+        }
+        set {
+            rawModifiedDate = newValue as NSDate?
+        }
+    }
+    
+    convenience init?(name: String?, content: String?) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate  //UIKit is needed to access UIApplication
+        guard let managedContext = appDelegate?.persistentContainer.viewContext,
+            let name = name, name != "" else {
+                return nil
+        }
+        self.init(entity: Document.entity(), insertInto: managedContext)
+        self.name = name
+        self.content = content
+        if let size = content?.count {
+            self.size = Int64(size)
+        } else {
+            self.size = 0
+        }
+        
+        self.modifiedDate = Date(timeIntervalSinceNow: 0)
+    }
+    
+    func update(name: String, content: String?) {
+        self.name = name
+        self.content = content
+        if let size = content?.count {
+            self.size = Int64(size)
+        } else {
+            self.size = 0
+        }
+    
+        self.modifiedDate = Date(timeIntervalSinceNow: 0)
+    }
+}
